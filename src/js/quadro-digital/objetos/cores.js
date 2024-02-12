5110d933e1c0aeb55894b5ba4da7d9efc5c0fcb6
Inacio Pereira <inaciospereira@gmail.com>
2024-02-12 17:33:13 -0300

    implementação do quadro-digital

---
+++ src/js/quadro-digital/objetos/cores.js
@@ -0,0 +1,13 @@
+const cores = {
+    vermelho: '#E4827A',
+    verde: '#9EE6B3',
+    amarelo: '#F9E96D',
+    roxo: '#DFA6F4',
+    azul: '#ABE0FE',
+    laranja: '#EDC066',
+    marrom: '#C68D67',
+    branco: '#F4F4F4',
+    preto: '#595959'
+}
+
+export { cores }
---
+++ src/js/quadro-digital/quadro.js
@@ -0,0 +1,42 @@
+import { cores } from "./objetos/cores.js"
+const paintModes = document.querySelectorAll(".pinceis button")
+const tintas = document.querySelectorAll(".paint-colors button")
+
+let modoSelecionado = 'pincel'
+let corSelecionada = cores['vermelho']
+
+console.log(corSelecionada)
+
+paintModes.forEach(paintMode => {
+    paintMode.addEventListener('click', () => {
+        if(paintMode.classList.contains('selected')){
+            return
+        }
+
+        const elementoSelecionado = document.querySelector('.pinceis .selected')
+        elementoSelecionado.firstElementChild.src = `../../../../src/images/quadro-digital/buttons/modes/${elementoSelecionado.name}.svg`
+        elementoSelecionado.classList.toggle('selected')
+
+        paintMode.classList.toggle('selected')
+        paintMode.firstElementChild.src = `../../../../src/images/quadro-digital/buttons/modes/${paintMode.name}-ativo.svg`
+
+        modoSelecionado = paintMode.name
+    })
+})
+
+
+tintas.forEach(tinta => {
+    tinta.addEventListener('click', () => {
+        if(tinta.classList.contains('selected')){
+            return
+        }
+
+        const elementoSelecionado = document.querySelector('.paint-colors .selected')
+        elementoSelecionado.classList.toggle('selected')
+
+        tinta.classList.toggle('selected')
+        corSelecionada = cores[tinta.name]
+
+        console.log(corSelecionada)
+    })
+})
